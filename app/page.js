789c150072a13d3e23b528b46f742601c656fe55
robertacintra <roberta.cintra@alumni.usp.br>
2023-06-06 14:45:42 -0300

    Implements modal with all anime details

--- app/page.js
+++ app/page.js
@@ -1,19 +1,27 @@
 "use client";
 import { useEffect, useState } from 'react';
+import { Card, Row, Col, Button, Modal, Descriptions } from 'antd';
 import axios from 'axios';
-import { Card, Row, Col, Button } from 'antd';
-import Link from 'next/link';
 
 const { Meta } = Card;
 
 const Home = () => {
     const [animes, setAnimes] = useState([]);
     const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedAnime, setSelectedAnime] = useState(null);
+
+    const handleLoadMore = async () => {
+        setPage(prevPage => prevPage + 1);
+    };
 
     useEffect(() => {
         fetchAnimes();
     }, [page]);
+
     const fetchAnimes = async () => {
+        setLoading(true);
         try {
             const response = await axios.get(`https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=${(page - 1) * 10}`);
             const newAnimes = response.data.data;
@@ -21,33 +29,69 @@ const Home = () => {
         } catch (error) {
             console.error('Error fetching animes:', error);
         }
+        setLoading(false);
     };
 
-    const handleLoadMore = () => {
-        setPage(prevPage => prevPage + 1);
+    const handleOpenModal = (anime) => {
+        setSelectedAnime(anime);
+        setModalVisible(true);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedAnime(null);
+        setModalVisible(false);
     };
 
     return (
-        <div>
-            <h1>Anime Catalogue</h1>
-            <Row gutter={[16, 16]}>
-                {animes.map(anime => (
-                <Col key={anime.id} xs={24} sm={12} md={8} lg={6} xl={4}>
-                    <Link href={`/anime/${anime.id}`} passHref>
-                    <Card
-                        hoverable
-                        cover={<img alt={anime.attributes.canonicalTitle} src={anime.attributes.posterImage.medium} />}
-                        style={{ cursor: 'pointer' }}
-                    >
-                        <Meta title={anime.attributes.canonicalTitle} description={anime.attributes.startDate} />
-                    </Card>
-                    </Link>
-                </Col>
-                ))}
-            </Row>
-            <Button onClick={handleLoadMore} type="primary" style={{ marginTop: '16px' }}>
-                Load More
-            </Button>
+        <div style={{ padding: '20px' }}>
+        <h1>Anime Catalogue</h1>
+        <Row gutter={[16, 16]}>
+            {animes.map((anime) => (
+            <Col key={anime.id} xs={24} sm={12} md={8} lg={6} xl={4}>
+                <Card
+                hoverable
+                cover={<img alt={anime.attributes.canonicalTitle} src={anime.attributes.posterImage.medium} />}
+                style={{ cursor: 'pointer' }}
+                onClick={() => handleOpenModal(anime)}
+                >
+                <Meta title={anime.attributes.canonicalTitle} description={anime.attributes.startDate} />
+                </Card>
+            </Col>
+            ))}
+        </Row>
+
+        {loading && <p>Loading...</p>}
+
+        {!loading && (
+            <div style={{ textAlign: 'center', marginTop: '20px' }}>
+            <Button onClick={handleLoadMore}>Load More</Button>
+            </div>
+        )}
+
+        <Modal
+            title={selectedAnime ? selectedAnime.attributes.canonicalTitle : ''}
+            visible={modalVisible}
+            onCancel={handleCloseModal}
+            footer={null}
+        >
+            {selectedAnime && (
+            <div>
+                <img alt={selectedAnime.attributes.canonicalTitle} src={selectedAnime.attributes.posterImage.medium} />
+                <Descriptions title="Anime Details" layout="vertical" bordered>
+                    <Descriptions.Item label="Synopsis">{selectedAnime.attributes.synopsis}</Descriptions.Item>
+                    <Descriptions.Item label="Start Date">{selectedAnime.attributes.startDate}</Descriptions.Item>
+                    <Descriptions.Item label="End Date">{selectedAnime.attributes.endDate}</Descriptions.Item>
+                    <Descriptions.Item label="Episode Count">{selectedAnime.attributes.episodeCount}</Descriptions.Item>
+                    <Descriptions.Item label="Episode Length">
+                    {selectedAnime.attributes.episodeLength} minutes
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Rating">{selectedAnime.attributes.averageRating}</Descriptions.Item>
+                    <Descriptions.Item label="Age Rating">{selectedAnime.attributes.ageRating}</Descriptions.Item>
+                    <Descriptions.Item label="Popularity Rank">{selectedAnime.attributes.popularityRank}</Descriptions.Item>
+                </Descriptions>
+            </div>
+            )}
+        </Modal>
         </div>
     );
 };
